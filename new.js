da29312a325e3f2bee48929d34333b983485b93f
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-01-14 15:51:49 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,9 @@
+console.log("Node.js Backend Started");
+let name = "Aman";
+let age= 22;
+const country ="India";
+let isStudent = true;
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
