36e982e859e7e300f3fdc9b60350f4054e948c1d
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-01-22 10:40:21 +0530

    Delete new.js

--- new.js
+++
@@ -1,9 +0,0 @@
-console.log("Node.js Backend Started");
-let name = "Aman";
-let age= 22;
-const country ="India";
-let isStudent = true;
-console.log(name);
-console.log(age);
-console.log(country);
-console.log(isStudent);
